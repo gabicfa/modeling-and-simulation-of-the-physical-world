ceb8d50ee9829a26b5b8b116b270602b8e98e80c
Gabriela Almeida <gabicfa1996@gmail.com>
2019-04-20 18:58:23 -0300

    translation

--- Project 1 - Population Dynamics/calcPopulation.py
+++ Project 1 - Population Dynamics/calcPopulation.py
@@ -9,17 +9,17 @@ import matplotlib.pyplot as plt
 from pylab import *
 from funcs import *
 
-def calcPopulation (gamaT, T, P, betaR, Tc, V, alfaV, Rc, tempo):
+def calcPopulation (gamaT, T, P, betaR, Tc, V, alfaV, Rc, time):
     
-    Ti=[0]*tempo
-    Ri=[0]*tempo
-    Vi=[0]*tempo
+    Ti=[0]*time
+    Ri=[0]*time
+    Vi=[0]*time
     Ti[0]= T
     Ri[0]= R
     Vi[0]= V
     f=1.9
 
-    for i in range(1,tempo):
+    for i in range(1,time):
         DT = CalcDeltaT(gamaT, Ti[i-1], f, P)
         Ti[i] = Ti[i-1] + DT
          
@@ -29,33 +29,33 @@ def calcPopulation (gamaT, T, P, betaR, Tc, V, alfaV, Rc, tempo):
         DV = CalcDeltaV(Vi[i-1], alfaV, Ri[i-1], Rc)
         Vi[i]= Vi[i-1] + DV
         
-    print(Ti[tempo-1])
-    print(Ri[tempo-1])
-    print(Vi[tempo-1])
+    print(Ti[time-1])
+    print(Ri[time-1])
+    print(Vi[time-1])
    
-    Tempo = range(0, tempo)  
+    Time = range(0, time)  
     
-    plt.plot(Tempo, Ti, 'b-')
-    plt.axis([0, tempo-1, 9000000, 30000000])
-    plt.xlabel('tempo [anos]')
-    plt.ylabel('T[populacao de tubaroes]')
-    plt.title('Populacao de Tubarao')
+    plt.plot(Time, Ti, 'b-')
+    plt.axis([0, time-1, 9000000, 30000000])
+    plt.xlabel('time [anos]')
+    plt.ylabel('T[population of sharks]')
+    plt.title('Population of Sharks')
     plt.show()
     
     
-    plt.plot(Tempo, Ri, 'g-')
-    plt.axis([0, tempo-1, 15000000, 50000000])
-    plt.xlabel('tempo [anos]')
-    plt.ylabel('R[populacao de raias]')
-    plt.title('Populacao de Rais')
+    plt.plot(Time, Ri, 'g-')
+    plt.axis([0, time-1, 15000000, 50000000])
+    plt.xlabel('time [anos]')
+    plt.ylabel('R[population of rays]')
+    plt.title('Population of Rays')
     plt.show()
     
     
-    plt.plot(Tempo, Vi, 'r-')
-    plt.axis([0, tempo-1, 70000000, 160000000])
-    plt.xlabel('tempo [anos]')
-    plt.ylabel('V[populacao de vieiras]')
-    plt.title('Populacao de vieiras')
+    plt.plot(Time, Vi, 'r-')
+    plt.axis([0, time-1, 70000000, 160000000])
+    plt.xlabel('time [anos]')
+    plt.ylabel('V[population of scallops]')
+    plt.title('Population of Scallops')
     plt.show()
     
     plt.plot()
@@ -72,7 +72,7 @@ Tc=12000000
 V= 80000000 
 alfaV=0.1
 Rc=23000000
-tempo= 30 
+time= 30 
 
     
-calcPopulation (gamaT, T, P, betaR, Tc, V, alfaV, Rc, tempo)
+calcPopulation (gamaT, T, P, betaR, Tc, V, alfaV, Rc, time)
